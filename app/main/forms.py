82595fc204381fa8e25eaeb8d0b657d238b64b46
shadykip <shady.kip@outlook.com>
2024-05-27 06:25:18 +0300

    Added bootstrap css and custom classes to forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
+from wtforms.widgets import TextInput, TextArea, SubmitInput
+
+
+class CustomTextInput(TextInput):
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("class", "form-control")
+        return super().__call__(field, **kwargs)
+
+
+class CustomTextArea(TextArea):
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("class", "form-control")
+        return super().__call__(field, **kwargs)
+
+
+class CustomSubmitInput(SubmitInput):
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("class", "btn btn-primary")
+        return super().__call__(field, **kwargs)
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    message = TextAreaField("Message", validators=[DataRequired()])
-    submit = SubmitField("Send")
+    name = StringField("Name", validators=[DataRequired()], widget=CustomTextInput())
+    email = StringField(
+        "Email", validators=[DataRequired(), Email()], widget=CustomTextInput()
+    )
+    message = TextAreaField(
+        "Message", validators=[DataRequired()], widget=CustomTextArea()
+    )
+    submit = SubmitField("Send", widget=CustomSubmitInput())
